3395a7dc638f365760903827c2b8e39cfec499bb
anuragpranav <anuragpranav@gmail.com>
2016-05-12 13:49:22 +0530

    Alternate solution to BITMAP 12/05/2016 1349hrs

--- BITMAP.cpp
+++ BITMAP.cpp
@@ -1,24 +1,71 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct couple
+{
+	int i, j;
+	couple(int y, int x)
+	{
+		i = y;
+		j = x;
+	}
+};
+
+couple make_couple(int i, int j)
+{
+	couple x(i, j);
+	return x;
+}
+
+inline int mod(int x)
+{
+	if (x < 0)
+		return -x;
+	return x;
+}
+
+inline int d(couple p1, couple p2)
+{
+	return mod(p1.i - p2.i) + mod(p1.j - p2.j);
+}
+
 int main()
 {
-	ios::sync_with_stdio(false);
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		int n, m;
-		cin>>n>>m;
-		bool map[n][m];
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)
-			{
-				cin>>map[i][j];
-				if (map[i][j])
-					white.push_back()
-			}
-	}
+		int N, M;
+		cin>>N>>M;
+		std::vector<couple> Ones;
+		bool Map[N][M];
+		char x;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M && cin>>x; j++)
+				if (x == '1')
+				{
+					Map[i][j] = 1;
+					Ones.push_back(make_couple(i, j));
+				}
+				else
+					Map[i][j] = 0;
+
+		for (int i = 0; i < N; i++)
+		{	
+			for (int j = 0; j < M; j++)
+				if (!Map[i][j])
+				{
+					int minDistance = 2000;
+					for (int x = 0; x < Ones.size(); x++)
+						minDistance = min(minDistance, d(Ones[x], make_couple(i, j)));
+					cout<<minDistance<<' ';
+				}
+				else
+					cout<<"0 ";
+			cout<<endl;
+		}
+	}	
 	return 0;
 }
---
+++ BITMAP_1.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int Map[182][182];
+
+struct couple
+{
+	int i, j;
+	couple(int y, int x)
+	{
+		i = y;
+		j = x;
+	}
+};
+
+couple MakeCouple(int i, int j)
+{
+	couple x(i, j);
+	return x;
+}
+
+void AssignMinDistance(int i, int j, int N, int M)
+{
+	int distance = 0;
+	queue<couple> ToVisit;
+	Map[i][j] = 0;
+	ToVisit.push(MakeCouple(i, j));	//root of BFS
+
+	while(ToVisit.size())
+	{		
+		int y = ToVisit.front().i;
+		int x = ToVisit.front().j;
+		couple Visiting(y, x);	//visit this node's neighbours
+		ToVisit.pop();	//remove the first element
+		distance = Map[y][x] + 1;	//exploring immediate neighbours
+		if (y != 0 && distance < Map[y-1][x])	//can visit up
+		{	
+			Map[y-1][x] = distance;
+			ToVisit.push(MakeCouple(y-1, x));
+		}
+
+		if (y != N-1 && distance < Map[y+1][x])	//can visit down
+		{
+			Map[y+1][x] = distance;
+			ToVisit.push(MakeCouple(y+1, x));
+		}
+
+		if (x != 0 && distance < Map[y][x-1])	//can visit left
+		{
+			Map[y][x-1] = distance;
+			ToVisit.push(MakeCouple(y, x-1));
+		}
+
+		if (x != M-1 && distance < Map[y][x+1])	//can visit right
+		{
+			Map[y][x+1] = distance;
+			ToVisit.push(MakeCouple(y, x+1));
+		}
+	}
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N, M;
+		cin>>N>>M;
+		std::vector<couple> Ones;
+		char x;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M && cin>>x; j++)
+				if (x == '1')
+				{
+					Map[i][j] = -2000;
+					Ones.push_back(MakeCouple(i, j));
+				}
+				else
+					Map[i][j] = 2000;	//shall be overwritten the first time it's accessed in BFS
+	
+		for (int i = 0; i < Ones.size(); i++)
+			AssignMinDistance(Ones[i].i, Ones[i].j, N, M);
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+				cout<<Map[i][j]<<' ';
+			cout<<endl;
+		}
+	}
+	return 0;
+}
