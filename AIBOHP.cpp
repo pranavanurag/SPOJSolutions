5302c8a392969e3105b91c8f2cac98d60b5fd323
Pranav Anurag <anuragpranav@gmail.com>
2016-05-20 17:20:39 +0530

    Update AIBOHP.cpp

--- AIBOHP.cpp
+++ AIBOHP.cpp
@@ -1,49 +1,31 @@
-/*input
-1
-tff
-*/
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int Memo[6100][6100];
-string x;
-
-void RefreshMemo()
-{
-	for (int i = 0; i < 6100; i++)
-		for (int j = 0; j < 6100; j++)
-			Memo[i][j] = -1;
-}
-
-bool IsPalin(string x)
-{
-	for (int i = 0; i < x.size(); i++)
-		if (x[i] != x[x.size()-i-1])
-			return 0;
-	return 1;
-}
-
-int MinPalin(int i, int j)
-{
-	if (Memo[i][j] >= 0)
-		return Memo[i][j];
-
-	else
-	{
-		
-	}
-}
+int Memo[6101][6101];
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	int n;
-	cin>>n;
-	while (n--)
+	int t;
+	cin>>t;
+	while (t--)
 	{
-		cin>>x;
-
+		for (int i = 0; i < 6101; i++)
+			for (int j = 0; j < 6101; j++)
+				Memo[i][j] = 0;
+		string S;
+		cin>>S;
+		int n = S.size();
+		for (int i = n-1; i >= 0 ; i--)
+			for (int j = 0; j <= n-1; j++)
+				if (i == j)
+					Memo[i][j] = 0;
+				else if (S[i] == S[j])
+					Memo[i][j] = Memo[i+1][j-1];
+				else
+					Memo[i][j] = min(Memo[i+1][j], Memo[i][j-1])+1;
+		cout<<Memo[0][n-1]<<endl;
 	}
+	return 0;
 }
