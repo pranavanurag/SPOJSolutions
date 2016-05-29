e79ed6318f955f12d3ce2ba9a1781672562f917c
anuragpranav <anuragpranav@gmail.com>
2016-05-29 17:56:51 +0530

    29/05/2016 1756hrs

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -1,69 +1,57 @@
-/*input
-2
-1 1000000
-*/
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <stdio.h>
 
 using namespace std;
 
-int x[43460], y[1000001];	//declaring the seive and the segment array
-unsigned long long int m, n;	//declaring the integer variables for the segment
+bool isPrime[46342];
+long L, U;
+bool A[1000001];
 
-void initialize()	//funtion to prepare the seive by assigning all prime representing indices with a value of 1
+void Print()
 {
-	for(int i=0;i<43459;i++)	//loop to assign all indices a composite marking
-		x[i]=1;
+	if (L == 1)
+		L++;
 
-	int prime=2;
-	for(int i=0;i<43459;i++)	//loop to create the seive
-	{
-		if(x[i]==1) prime=i+2; //picking up primes to cross out their multiples
+	int N = U - L + 1;
+	for (int i = 1; i <= N; i++)
+		A[i] = 1;
 
-		for(int j=prime*prime-2;j<43459;j+=prime)
-			x[j]=0;    //crossing out all the multiples of th prime number picked up in the previous loop
-	}
+	for (int i = 2; i <= sqrt(U); i++)
+		if (isPrime[i])
+		{
+			int Prime = i;
+			for (int j = (Prime - L%Prime)%Prime + 1; j <= N; j += Prime)
+				if (j+L-1 != Prime)
+					A[j] = 0;
+		}
+
+	for (int i = 1; i <= N; i++)
+		if (A[i])
+			printf("%ld\n", i+L-1);
 }
 
-void printprimes(unsigned long long int m, unsigned long long int n)	//function to seive out all the primes in the segment [m,n]
+void Seive()
 {
-	if(m==1) m+=1;	//incrementing m to 2 if it is 1 to begin with
+	for (int i = 2; i <= 46341; i++)
+		isPrime[i] = 1;
 
-	int n1=n-m+1;	//n1 is the size of the segment that we have to work with
-	int prime;
-	
-	for(int i=0;i<n1;i++)	//loop to initialize the segment
-		y[i]=1;
-
-	for(int i=0;i<sqrt(n)-2;i++)	//loop to cross out multiples in the segment [m,n] of primes from 2 to square root of n
-	{
-		if(x[i]==1) prime=i+2;
-		
-		unsigned long long int begin=prime-(m%prime);	//initializing for the starting point to start crossing out multiples in the segmented of the current prime
-		begin=begin%prime;	//to avoid begin attaining the value of prime itself
-		
-		for(int j=begin;j<n1;j+=prime)    //crossing out all the multiples of prime in the segment [m,n]
-			if(j+m!=prime)	//checking if the number being crossed out is not the prime itself
-				y[j]=0;
-	}
-
-	for(int i=0;i<n1;i++)
-		if(y[i]==1) cout<<m+i<<endl;	//printing out all the numbers corressponding to a stored value of 1 (prime)
+	for (int i = 2; i <= 216; i++)
+		if (isPrime[i])
+			for (int j = i*i; j <= 46341; j += i)
+				isPrime[j] = 0;
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
-    initialize();	//the seive is now ready
-
-    int t;
-    cin>>t;    //number of test cases
-
-    for(int i=0;i<t;i++)	//loop to run the printprimes function t times
-    {
-    	cin>>m>>n;
-    	printprimes(m,n);	//calling the printprimes(,) function
-    }	
-
-    return 0;
+	Seive();
+	int t;
+	scanf("%d", &t);
+	while (t--)
+	{
+		scanf("%ld %ld", &L, &U);
+		Print();
+	}
+	return 0;
 }
